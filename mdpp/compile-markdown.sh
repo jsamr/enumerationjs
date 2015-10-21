838db6296af19e52aa9e189879a4742854461240
ranj2004 <jrandolph@orange.fr>
2015-10-21 13:17:17 -0400

    Doc fixes

--- mdpp/compile-markdown.sh
+++ mdpp/compile-markdown.sh
@@ -2,6 +2,6 @@
 # This script compiles markdown pre processor files (.mdpp) to README.MD and README.JS.MD files.
 # See the markdown-pp github project here : https://github.com/jreese/markdown-pp
 $(cd ../ && echo "✓ Last Jasmine test run was the $(date -u "+%D at %R") UTC  with " > mdpp/lastbuild.mdpp && echo **$(jasmine | sed -e '$!{h;d;}' -e x)** >> mdpp/lastbuild.mdpp)
-markdown-pp js_index.mdpp -o ../JS.GUIDE.MD
-markdown-pp coffee_index.mdpp -o ../COFFEE.GUIDE.MD
+markdown-pp js-guide.mdpp -o ../JS.GUIDE.MD
+markdown-pp coffee-guide.mdpp -o ../COFFEE.GUIDE.MD
 markdown-pp readme.mdpp -o ../README.MD
