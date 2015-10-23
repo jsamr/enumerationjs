591039a5e1475e837df7cb5db53e9c51f834f817
ranj2004 <jrandolph@orange.fr>
2015-10-23 17:17:12 -0400

    Created jasmine report badge

--- mdpp/compile-markdown.sh
+++ mdpp/compile-markdown.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 # This script compiles markdown pre processor files (.mdpp) to README.MD and README.JS.MD files.
+# It also generates the specs badge.
 # See the markdown-pp github project here : https://github.com/jreese/markdown-pp
 cd "${BASH_SOURCE%/*}" || exit 1
-travisTest='![Travis build](https://travis-ci.org/sveinburne/enumeration.js.svg?branch=master) ![dependencies](https://david-dm.org/sveinburne/enumeration.js.svg)'
 if [[ ! -f js-guide.mdpp ]] || [[ ! -f coffee-guide.mdpp ]] || [[ ! -f readme.mdpp ]]; then
     echo "Missing mdpp files !"
 fi
-$(cd ../ && echo "$travisTest" > mdpp/lastbuild.mdpp && echo **$(jasmine | sed -e '$!{h;d;}' -e x)** >> mdpp/lastbuild.mdpp)
+xmlstarlet --version > /dev/null 2>&1
+if [[ $? -eq 127 ]] ; then echo "xmlstarlet is not installed" && exit 1;
+markdown-pp > /dev/null 2>&1
+fi
+if [[ $? -eq 127 ]] ; then echo "markdown-pp is not installed" && exit 1;
+fi
+reportFile=jasmine-report.svg
+jasmineReport=$(cd ../ && jasmine | sed -e '$!{h;d;}' -e x)
+reports=(${jasmineReport//,/ })
+style=""
+failures=${reports[@]:2:2}
+specs=${reports[@]:0:2}
+pwd
+xmlstarlet ed -L -N svg="http://www.w3.org/2000/svg" -u '//svg:defs/svg:text[@id="failures"]' -v "$failures"  $reportFile
+xmlstarlet ed -L -N svg="http://www.w3.org/2000/svg" -u '//svg:defs/svg:text[@id="specs"]' -v "$specs"  $reportFile
+if [[ ${reports[2]}  -eq 0 ]]; then style="fill:#44cc11;"
+else style="fill:#e05d44;"
+fi
+xmlstarlet ed -L -N svg="http://www.w3.org/2000/svg" -u '//svg:*[@class="wrapped"]/@style' -v $style  $reportFile
+
+
+
 markdown-pp js-guide.mdpp -o ../JS.GUIDE.MD
 markdown-pp coffee-guide.mdpp -o ../COFFEE.GUIDE.MD
 markdown-pp readme.mdpp -o ../README.MD
