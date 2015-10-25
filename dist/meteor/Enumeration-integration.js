adc1bcb16095abc85c58cc343a088262510a85e2
ranj2004 <jrandolph@orange.fr>
2015-10-24 22:25:26 -0400

    Meteor distribution

---
+++ dist/meteor/Enumeration-integration.js
@@ -0,0 +1,5 @@
+// Write your tests here!
+// Here is an example.
+Tinytest.add('Assert Global object Enumeration is defined', function (test) {
+  test.ok(Enumeration);
+});
---
+++ dist/meteor/package.js
@@ -0,0 +1,23 @@
+Package.describe({
+  name: 'sveinburne:enumerationjs',
+  version: '1.3.4',
+  // Brief, one-line summary of the package.
+  summary: 'Java-like super flexible enums ! Move the logic and refactor.',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/sveinburne/enumerationjs/',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.MD'
+});
+
+Package.onUse(function(api) {
+  api.use('underscore', ['client', 'server']);
+  api.addFiles('Enumeration.js', ['client', 'server']);
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('sveinburne:enumerationjs');
+  api.addFiles('Enumeration-integration.js');
+  api.export("Enumeration");
+});
