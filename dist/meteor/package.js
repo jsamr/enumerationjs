884a4e6876acbb5f6d80ff177beee96fe1182e49
ranj2004 <jrandolph@orange.fr>
2015-11-04 02:34:13 -0500

    badge fix

--- dist/meteor/package.js
+++ dist/meteor/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'sveinburne:enumerationjs',
-  version: '1.3.11',
+  version: '1.3.13',
   // Brief, one-line summary of the package.
   summary: 'Java-like super flexible enums ! Move the logic and refactor.',
   // URL to the Git repository containing the source code for this package.
